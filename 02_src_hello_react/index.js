f6337629fda9364a770eed38873fb6cdcccccbff
xcc <2045659302@qq.com>
2021-02-18 04:13:29 +0800

    阿达

--- 02_src_hello_react/index.js
+++ 02_src_hello_react/index.js
@@ -2,7 +2,8 @@
 import React from 'react'
 //引入ReactDOM
 import ReactDOM from 'react-dom'
-//引入App
+//引入App组件
 import App from './App'
 
+//渲染App到页面
 ReactDOM.render(<App/>,document.getElementById('root'))
